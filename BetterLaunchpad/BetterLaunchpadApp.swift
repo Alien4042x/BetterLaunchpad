62671530c46433a65628d21838c658d20fd2f8a8
Radim Vesely <bright4042x@yahoo.com>
2025-09-17 15:17:32 +0200

    Initial Commit

---
+++ BetterLaunchpad/BetterLaunchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterLaunchpadApp.swift
+//  BetterLaunchpad
+//
+//  Created by Radim Veselý on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterLaunchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
